e8336cd62962c0b477798f0bb04484cc69f976f3
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-02-14 01:23:37 -0800

    Cleaning up code and comments

--- chessboard.js
+++ chessboard.js
@@ -22,29 +22,29 @@ function createPositionSettings(positionIn) {
 /**
  * Write out the rendered template file as an .html file
  */
-function writeTemplate(position, boardTemplateFileName, boardFileName) {
+function writeBoardTemplate(position, boardTemplateFile, boardHtmlFile) {
 
   swig.setDefaults({ cache: false, autoescape: false });
 
   // Compile the board template file
-  var tpl = swig.compileFile(boardTemplateFileName);
+  var tpl = swig.compileFile(boardTemplateFile);
 
   // Render the position
   var rendered = tpl(position);
 
   // Write out the board file
-  fs.writeFile(boardFileName, rendered, function(err) {
+  fs.writeFile(boardHtmlFile, rendered, function(err) {
     if (err) {
       return console.log(err);
     }
-    console.log('writting '+boardFileName+' done');
+    console.log('writting '+boardHtmlFile+' done');
   });
 }
 
 /**
  * Render the board html file as an image file (png)
  */
-function renderBoard(boardFileName, boardImageFileName) {
+function renderBoardImage(boardHtmlFile, boardImageFile) {
 
   phantom.create("--ignore-ssl-errors=yes", "--ssl-protocol=any", "--web-security=no", function (ph) {
     ph.createPage(function (page) {
@@ -52,11 +52,11 @@ function renderBoard(boardFileName, boardImageFileName) {
       page.set('viewportSize', {width:1280,height:900}, function(){
         page.set('clipRect', {top:0,left:0,width:1280,height:900}, function(){
           // open page
-          page.open(boardFileName, function(status) {
+          page.open(boardHtmlFile, function(status) {
             // wait 1.5 seconds for webpage to be completely loaded
             setTimeout(function(){
-              page.render(boardImageFileName, function(finished){
-                console.log('rendering '+boardFileName+' as '+boardImageFileName+' done');
+              page.render(boardImageFile, function(finished){
+                console.log('rendering '+boardHtmlFile+' as '+boardImageFile+' done');
                 ph.exit();
               });
             }, 1500);
@@ -77,17 +77,17 @@ function renderBoard(boardFileName, boardImageFileName) {
  *   - Render the board html file as an image
  */
 
-var boardTemplateFileName = "chessboard.tpl.html";
-var boardFileName = "chessboard.html";
-var boardImageFileName = "chessboard.png";
+var boardTemplateFile = "chessboard.tpl.html";
+var boardHtmlFile = "chessboard.html";
+var boardImageFile = "chessboard.png";
 
 var position = createPositionSettings({
   h1: "bB",
   b1: "wQ",
+  c2: "wQ",
+  d3: "wQ",
   g8: "wK"
 });
 
-writeTemplate(position, boardTemplateFileName, boardFileName);
-renderBoard(boardFileName, boardImageFileName);
-
-
+writeBoardTemplate(position, boardTemplateFile, boardHtmlFile);
+renderBoardImage(boardHtmlFile, boardImageFile);
