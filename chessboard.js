1d1c61e79f7ffb24c9151914b32649d5778ed4cf
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-02-14 00:01:22 -0800

    Added templated chessboard using swig and fs

--- chessboard.js
+++ chessboard.js
@@ -1,6 +1,28 @@
 var phantom = require('phantom');
+var swig  = require('swig');
+var fs = require('fs');
 
 var pageUrl = "chessboard.html";
+
+var imagePrefix = "<img src='img/";
+var imageSuffix = ".png'>";
+
+swig.setDefaults({ cache: false, autoescape: false });
+
+// Compile a file and store it, rendering it later
+var tpl = swig.compileFile('chessboard.tpl.html');
+
+var rendered = tpl({
+  a3: imagePrefix + "bB" + imageSuffix,
+  b8: imagePrefix + "wK" + imageSuffix
+});
+
+fs.writeFile(pageUrl, rendered, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('writting '+pageUrl+' done');
+});
 
 phantom.create("--ignore-ssl-errors=yes", "--ssl-protocol=any", "--web-security=no", function (ph) {//mMAKE SURE WE CAN RENDER https
   ph.createPage(function (page) {
